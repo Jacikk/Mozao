3648b5a71eb27a82213cfd8c0e2de32495eb2bbc
jacikk <vinicius-jacik@hotmail.com>
2020-05-22 23:06:14 -0300

    agora vai

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../src/Img/coracao.png'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Te amo mozão, desculpa não ir ai
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
